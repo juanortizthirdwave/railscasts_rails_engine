973da3193d91911a52d2542266eed0faf176d879
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-08-20 12:32:40 -0500

    added style, javascript, and a redirect

--- test/dummy/app/controllers/static_pages_controller.rb
+++ test/dummy/app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    flash[:message] = "redirecting from the host app"
+    # using 'uhoh' you can access the route of your engine
+    redirect_to uhoh.root_url 
   end
 end
