044997a17d2d3252dcb8918991c73293579044ae
Juan Ortiz <juan.ortiz@thirdwavellc.com>
2014-08-20 11:38:30 -0500

    removed the counter in exception handler

--- config/initializers/exception_handler.rb
+++ config/initializers/exception_handler.rb
@@ -1,8 +1,7 @@
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, start, finish, id, payload|
-  counter = 1
+
   if payload[:exception]
     name, msg = *payload[:exception]
-    Uhoh::Failure.create!(:message => "#{msg}  n.#{counter}")
-    counter += 1
+    Uhoh::Failure.create!(:message => msg)
   end
 end
